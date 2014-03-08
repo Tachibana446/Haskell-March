f9f106ee9f86dcd391bc4fc2f510465cc7e2fc9c
Tachibana446 <stinger33z@gmail.com>
2014-03-08 15:51:17 +0900

    文字列から大文字を除去する関数

--- baby.hs
+++ baby.hs
@@ -1,5 +1,1 @@
-doubleMe x = x + x
-doubleUs x y = doubleMe x + doubleMe y
-doubleSmallNumber x = if x > 100
-						then x
-						else x * 2
+removeNonUppercase st = [c | c <- st , c `elem` ['A' .. 'Z']]
