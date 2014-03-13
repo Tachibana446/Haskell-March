71da414de99a776fd233fcedbaf141a1f91fca44
Tachibana446 <stinger33z@gmail.com>
2014-03-14 03:49:45 +0900

    p47 体重と身長のペアのリストからBMIのリストを返す関数をlet式を使って書きなおしてみる

--- baby.hs
+++ baby.hs
@@ -10,6 +10,5 @@ bmiTell weight height
           fat = 30.0 
 
 calcBmis :: [(Double , Double)] -> [Double]
-calcBmis xs = [bmi w h | (w,h) <- xs]
-    where bmi weight height = weight / height ^ 2
+calcBmis xs = [bmi | (w,h) <- xs, let bmi = w / h ^ 2 ]
  
