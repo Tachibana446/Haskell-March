f51b4c8a9f304a947dc3932d517e7c7a7fe172c1
Tachibana446 <stinger33z@gmail.com>
2014-03-14 03:15:14 +0900

    p44 whereキーワードで更に痩せている・標準・太っている基準も変数に束縛
    where節のブロックはインデントをそろえないとエラーになる。

--- baby.hs
+++ baby.hs
@@ -1,7 +1,11 @@
 bmiTell :: Double -> Double -> String
 bmiTell weight height 
-    | bmi <= 18.5 = "You're underweight, you emo, you!"
-    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
-    | bmi <= 30.0 = "you're fat! Lose some weight, fatty!"
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "you're fat! Lose some weight, fatty!"
     | otherwise = "You're a whale, congratulations!"
-    where bmi = weight / height ^ 2 
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0 
+ 
