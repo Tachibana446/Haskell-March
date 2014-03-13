c9336aad1b98f4d1745bb029bb6378befc6c9eb0
Tachibana446 <stinger33z@gmail.com>
2014-03-14 03:52:28 +0900

    p47 フィルタを追加して太っている人のBMIだけを返すように変更

--- baby.hs
+++ baby.hs
@@ -10,5 +10,5 @@ bmiTell weight height
           fat = 30.0 
 
 calcBmis :: [(Double , Double)] -> [Double]
-calcBmis xs = [bmi | (w,h) <- xs, let bmi = w / h ^ 2 ]
+calcBmis xs = [bmi | (w,h) <- xs, let bmi = w / h ^ 2 , bmi > 25.0]
  
