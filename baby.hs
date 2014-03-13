3710dfb51cbbbc2d2678a17e47436679507dbe94
Tachibana446 <stinger33z@gmail.com>
2014-03-14 04:00:03 +0900

    p49 case式を使った関数の定義
    リストを受け取り、"The list is "という文字列にリストの大きさごとにcase式で
    判定された文字列をくっつけて返す

--- baby.hs
+++ baby.hs
@@ -1,14 +1,6 @@
-bmiTell :: Double -> Double -> String
-bmiTell weight height 
-    | bmi <= skinny = "You're underweight, you emo, you!"
-    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
-    | bmi <= fat    = "you're fat! Lose some weight, fatty!"
-    | otherwise = "You're a whale, congratulations!"
-    where bmi = weight / height ^ 2
-          skinny = 18.5
-          normal = 25.0
-          fat = 30.0 
-
-calcBmis :: [(Double , Double)] -> [Double]
-calcBmis xs = [bmi | (w,h) <- xs, let bmi = w / h ^ 2 , bmi > 25.0]
+describeList :: [a] -> String
+describeList ls = "The list is "
+                  ++ case ls of [] -> "Empty."
+                                [x] -> "a singleton list."
+                                xs -> "a longer list."
  
