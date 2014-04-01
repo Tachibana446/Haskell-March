a5ec78827a0aac11502b27aceff7000160e75e3a
Tachibana446 <stinger33z@gmail.com>
2014-04-01 22:20:59 +0900

    p67 高階プログラミングを使って標準関数を定義してみる(refs #4)

--- baby.hs
+++ baby.hs
@@ -1,3 +1,12 @@
-divideByTen :: (Floating a) => a -> a
-divideByTen = (/10)
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f x y = f y x
+
 
