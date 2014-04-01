5c0eed299dabb5a27d589fcd16662463de5cfbbf
Tachibana446 <stinger33z@gmail.com>
2014-04-02 08:29:43 +0900

    p72 長さ15以上のコラッツ列の開始数を計算する関数を定義(refs #5)

--- baby.hs
+++ baby.hs
@@ -1,12 +1,11 @@
-applyTwice :: (a -> a) -> a -> a
-applyTwice f x = f (f x)
+-- 1から100までの数のウチ、長さ15以上のコラッツ列の開始数になるものはいくつか
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | odd  n = n : chain (n * 3 + 1)
 
-zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' _ [] _ = []
-zipWith' _ _ [] = []
-zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
-
-flip' :: (a -> b -> c) -> (b -> a -> c)
-flip' f x y = f y x
-
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
 
