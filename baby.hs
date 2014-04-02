4b7cf57dd8d9d7081458c08b405cdaae510eb821
Tachibana446 <stinger33z@gmail.com>
2014-04-02 09:59:18 +0900

    p76 畳み込みを使ってsum関数を定義してみる(refs 7)

--- baby.hs
+++ baby.hs
@@ -1,10 +1,2 @@
--- 1から100までの数のウチ、長さ15以上のコラッツ列の開始数になるものはいくつか
-chain :: Integer -> [Integer]
-chain 1 = [1]
-chain n
-    | even n = n : chain (n `div` 2)
-    | odd  n = n : chain (n * 3 + 1)
-
-numLongChains :: Int
-numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
-
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x ->  acc + x) 0 xs
